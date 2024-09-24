eb3c1d448de60fdbd2601282018fb7773a585695
dannnluvs <110661011+dannnluvs@users.noreply.github.com>
2024-09-24 17:52:31 +0800

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name, school, course;
+    
+    cout << "Enter your Name: ";
+    cin >> name;
+    
+    cout  <<"Enter your School: ";
+    cin >> school;
+    
+    cout << "Enter your Course: ";
+    cin >> course;
+    
+    cout << "hello " << name << " your are studing at " << school << " and your course is "
+    << course;
+    
+    return 0;
+    
+}
